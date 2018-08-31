6abc5aea09c6ae9a2654e40e40e63a1f3ec5f6ab
周佳 <enxibaba@MacBook-Pro.local>
2018-08-31 23:54:06 +0800

    Beautify the pages and add navigation

--- Sources/App/Controllers/WebsiteController.swift
+++ Sources/App/Controllers/WebsiteController.swift
@@ -8,11 +8,13 @@ struct WebsiteController: RouteCollection {
         //Register indexHandler(_:) to process GET requests to the router's
         //root path,i.e.,a request to /.
         router.get(use: indexHandler)
-        router.get("acronyms", Acronym.parameter,use: acronymHandler)
+        router.get("acronyms", Acronym.parameter, use: acronymHandler)
+        router.get("users", User.parameter, use: userHandler)
+        router.get("users", use: allUsersHandler)
     }
-    
+
     func indexHandler(_ req: Request) throws -> Future<View> {
-        
+
         //Use Fluent query to get all the acronyms from the database.
         return Acronym.query(on: req)
                       .all()
@@ -26,7 +28,7 @@ struct WebsiteController: RouteCollection {
                         return try req.view().render("index", context)
         }
     }
-    
+
     func acronymHandler(_ req: Request) throws -> Future<View> {
         return try req.parameters.next(Acronym.self)
             .flatMap(to: View.self) { acronym in
@@ -40,6 +42,32 @@ struct WebsiteController: RouteCollection {
                 }
         }
     }
+
+    func userHandler(_ req: Request) throws -> Future<View> {
+
+        return try req.parameters.next(User.self)
+            .flatMap(to: View.self) { user in
+                return try user.acronyms
+                    .query(on: req)
+                    .all()
+                    .flatMap(to: View.self) { acronyms in
+                        let context = UserContext(
+                        title: user.name,
+                        user: user,
+                        acronyms: acronyms)
+                        return try req.view().render("user", context)
+                }
+        }
+    }
+
+    func allUsersHandler(_ req: Request) throws -> Future<View> {
+        return try User.query(on: req)
+                       .all()
+                       .flatMap(to: View.self) { users in
+                            let context = AllUsersContext(title: "All users", users: users)
+                            return try req.view().render("allUsers", context)
+                       }
+    }
 }
 
 struct IndexContext: Encodable {
@@ -47,8 +75,19 @@ struct IndexContext: Encodable {
     let acronyms: [Acronym]?
 }
 
-struct AcronymContext: Encodable{
+struct AcronymContext: Encodable {
     let title: String
     let acronym: Acronym
     let user: User
+}
+
+struct UserContext: Encodable {
+    let title: String
+    let user: User
+    let acronyms: [Acronym]
+}
+
+struct AllUsersContext: Encodable {
+    let title: String
+    let users: [User]
 }
